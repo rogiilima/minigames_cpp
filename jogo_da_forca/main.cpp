b09d6b0415e09785a1e8a615917bf44be70c0add
Igor Lima <igor.pflima@gmail.com>
2025-08-14 13:33:03 -0300

    Menu inicial jogo da forca

--- jogo_da_forca/main.cpp
+++ jogo_da_forca/main.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <windows.h>
+
+using namespace std;
+void limpa_tela(){
+    system("cls");
+}
+
+int main(){
+    int opcao = 0;
+    
+    
+    limpa_tela();
+    cout << "Bem-vindo ao Jogo da Forca!" << endl;
+    cout << "Escolha uma opcao:" << endl;
+    cout << "1. Jogar" << endl;
+    cout << "2. Jogar em Dupla" << endl;
+    cout << "3. Sobre" << endl;
+    cout << "4. Sair" << endl; 
+    
+    
+
+    return 0;   
+}
